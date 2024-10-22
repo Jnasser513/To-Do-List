fc5fd9fc16ecb45a7b17c092377f74deab97cad0
jnasser <josenasser@latmobile.com>
2024-10-21 20:27:17 -0600

    Create methods to extract total incomes, expenses and balance

--- to_do_list/lib/src/presentation/provider/to_do_provider.dart
+++ to_do_list/lib/src/presentation/provider/to_do_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:to_do_list/src/core/database/todo_database.dart';
 import 'package:to_do_list/src/data/to_do.dart';
+import 'package:to_do_list/src/data/to_do_type.dart';
 
 class ToDoProvider with ChangeNotifier {
   List<ToDo> todoList = [];
@@ -24,5 +25,21 @@ class ToDoProvider with ChangeNotifier {
   void removeToDoFromList(int id) {
     ToDoDatabase.db.deleteTransaction(id);
     fetchTodoList();
+  }
+
+  double get totalIncome {
+    return todoList
+        .where((todo) => todo.type == ToDoType.income && todo.amount > 0)
+        .fold(0.0, (sum, todo) => sum + todo.amount);
+  }
+
+  double get totalExpenses {
+    return todoList
+        .where((todo) => todo.type == ToDoType.expense && todo.amount > 0)
+        .fold(0.0, (sum, todo) => sum + todo.amount);
+  }
+
+  double get balance {
+    return totalIncome + totalExpenses;
   }
 }
